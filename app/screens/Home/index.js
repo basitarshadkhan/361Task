9083d5c1224a0214bc935d11461c4bdba6a19a94
Basit Arshad <basit.arshad@graana.com>
2021-05-26 03:06:47 +0500

    first screen designed!

--- app/screens/Home/index.js
+++ app/screens/Home/index.js
@@ -1,19 +1,74 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { Button } from 'react-native-paper';
+import React, { useState } from 'react';
+import { ScrollView, Text, Image, View, TouchableOpacity } from 'react-native';
+import Ellipsis from '../../assets/ellipsis.png';
+import Next from '../../assets/next.png';
 
-import { useDispatch } from 'react-redux';
-import * as loginActions from 'app/store/actions/loginActions';
 import styles from './styles';
 
 const Home = () => {
-  const dispatch = useDispatch();
-  const onLogout = () => dispatch(loginActions.logOut());
+  const initialValue = [
+    {
+      checked: require('../../assets/checked.png'),
+      img: require('../../assets/pregnant.png'),
+      title: 'Track My Cycle',
+      description: 'Predictions to increase the probability of pregnancy find.',
+    },
+    {
+      img: require('../../assets/pad.png'),
+      title: 'Track My Pregnancy',
+      description: 'Keep track of my symptoms,mood and health.',
+    },
+    {
+      checked: require('../../assets/checked.png'),
+      img: require('../../assets/pregnant.png'),
+      title: 'Track My Cycle',
+      description: 'Predictions to increase the probability of pregnancy find.',
+    },
+  ];
+  const [options] = useState(initialValue);
 
   return (
-    <View style={styles.container}>
-      <Text>Home Page</Text>
-    </View>
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+      <View style={styles.topContainer}>
+        <Image source={Ellipsis} style={styles.ellipsis} />
+        <Text style={styles.skipText}>Skip</Text>
+      </View>
+      <View style={styles.subContainer}>
+        <Text style={styles.holaText}>Hola!</Text>
+        <Text style={styles.holaText}>I am your assistant.</Text>
+        <Text style={[styles.assistText, { marginTop: 20 }]}>
+          To assist you all part of your body.
+        </Text>
+        <Text style={styles.assistText}>Select what bring you here.</Text>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={{ marginTop: 30 }}>
+          {options.map((item, index) => (
+            <View
+              style={[
+                styles.cardContainer,
+                { marginLeft: index !== 0 ? 10 : null },
+              ]}>
+              <View style={styles.optionsIconContainer}>
+                <View style={styles.imageContainer}>
+                  <Image source={item.img} style={styles.image} />
+                </View>
+                {item.checked && (
+                  <Image source={item.checked} style={styles.checkedIcon} />
+                )}
+              </View>
+              <Text style={styles.titleText}>{item.title}</Text>
+              <Text style={styles.descriptionText}>{item.description}</Text>
+            </View>
+          ))}
+        </ScrollView>
+        <TouchableOpacity style={styles.continueBtnContainer}>
+          <Text>Continue</Text>
+          <Image source={Next} style={styles.nextIconText} />
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
--- app/screens/Home/styles.js
+++ app/screens/Home/styles.js
@@ -3,8 +3,72 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#FAEDED',
+  },
+  topContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50,
+  },
+  ellipsis: {
+    resizeMode: 'contain',
+    width: 30,
+    height: 20,
+  },
+  skipText: { position: 'absolute', right: 20, fontWeight: 'bold' },
+  subContainer: { marginHorizontal: '5%', marginTop: 30, width: '90%' },
+  holaText: { fontSize: 40, fontWeight: 'bold' },
+  assistText: { color: '#5c5c5c', fontSize: 16 },
+  cardContainer: {
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    width: 170,
+    paddingHorizontal: 15,
+    paddingVertical: 40,
+  },
+  optionsIconContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  imageContainer: {
+    borderRadius: 50,
+    backgroundColor: '#FCD9D9',
+    width: 70,
+    height: 70,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  image: {
+    width: 40,
+    height: 40,
+    resizeMode: 'contain',
+  },
+  checkedIcon: {
+    width: 18,
+    height: 18,
+    resizeMode: 'contain',
+    // alignSelf: 'flex-end',
+  },
+  titleText: { fontSize: 24, fontWeight: '500', marginTop: 15 },
+  descriptionText: { color: '#5c5c5c', marginTop: 15, fontSize: 14 },
+  continueBtnContainer: {
+    flex: 1,
+    height: 50,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FCD9D9',
+    borderRadius: 10,
+    marginVertical: 30,
+  },
+  nextIconText: {
+    resizeMode: 'contain',
+    width: 13,
+    height: 13,
+    position: 'absolute',
+    right: 10,
   },
 });
 
